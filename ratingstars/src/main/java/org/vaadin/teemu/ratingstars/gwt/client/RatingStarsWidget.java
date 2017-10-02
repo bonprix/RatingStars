e3cdcb1eb079a29f941ac6a9123de53bdb44948f
vagharshakus <vagharshak.baghdasaryan@bonprix.net>
2017-10-02 12:16:41 +0200

    FW-334 solution for resting to zero implemented

--- ratingstars/src/main/java/org/vaadin/teemu/ratingstars/gwt/client/RatingStarsWidget.java
+++ ratingstars/src/main/java/org/vaadin/teemu/ratingstars/gwt/client/RatingStarsWidget.java
@@ -18,11 +18,10 @@ import com.google.gwt.user.client.ui.HasAnimation;
 import com.google.gwt.user.client.ui.HasValue;
 
 /**
- * RatingStarsWidget is the client-side implementation of the RatingStars
- * component.
- * 
+ * RatingStarsWidget is the client-side implementation of the RatingStars component.
+ *
  * The DOM tree for this component is constructed as follows:
- * 
+ *
  * <pre>
  *    div.v-ratingstars-wrapper
  *        div.v-ratingstars
@@ -33,22 +32,19 @@ import com.google.gwt.user.client.ui.HasValue;
  *            div.v-ratingstars-star
  *            div.v-ratingstars-bar
  * </pre>
- * 
- * The idea behind the DOM tree is that {@code .v-ratingstars-star} elements
- * always have a partially transparent background image and the width of the
- * {@code .v-ratingstars-bar} element behind these star elements is changed
- * according to the current value.
- * 
+ *
+ * The idea behind the DOM tree is that {@code .v-ratingstars-star} elements always have a partially transparent background image and the width of the
+ * {@code .v-ratingstars-bar} element behind these star elements is changed according to the current value.
+ *
  * @author Teemu Pöntelin
  */
-public class RatingStarsWidget extends FocusWidget implements HasAnimation,
-        HasValue<Double>, HasValueChangeHandlers<Double> {
+public class RatingStarsWidget extends FocusWidget implements HasAnimation, HasValue<Double>, HasValueChangeHandlers<Double> {
 
     /** Set the CSS class names to allow styling. */
     public static final String CLASSNAME = "v-ratingstars";
-    public static final String STAR_CLASSNAME = CLASSNAME + "-star";
-    public static final String BAR_CLASSNAME = CLASSNAME + "-bar";
-    public static final String WRAPPER_CLASSNAME = CLASSNAME + "-wrapper";
+    public static final String STAR_CLASSNAME = RatingStarsWidget.CLASSNAME + "-star";
+    public static final String BAR_CLASSNAME = RatingStarsWidget.CLASSNAME + "-bar";
+    public static final String WRAPPER_CLASSNAME = RatingStarsWidget.CLASSNAME + "-wrapper";
 
     private static final int ANIMATION_DURATION_IN_MS = 150;
 
@@ -67,35 +63,35 @@ public class RatingStarsWidget extends FocusWidget implements HasAnimation,
     private boolean readonly;
 
     public RatingStarsWidget() {
-        setElement(Document.get().createDivElement());
-        setStyleName(WRAPPER_CLASSNAME);
+        setElement(Document.get()
+            .createDivElement());
+        setStyleName(RatingStarsWidget.WRAPPER_CLASSNAME);
         initDom();
     }
 
     private void initDom() {
-        if (element != null) {
+        if (this.element != null) {
             // Remove previous element.
-            getElement().removeChild(element);
+            getElement().removeChild(this.element);
         }
 
-        element = Document.get().createDivElement();
-        element.setClassName(CLASSNAME);
-        getElement().appendChild(element);
+        this.element = Document.get()
+            .createDivElement();
+        this.element.setClassName(RatingStarsWidget.CLASSNAME);
+        getElement().appendChild(this.element);
 
         createStarElements();
 
-        barDiv = createBarDiv();
-        element.appendChild(barDiv);
+        this.barDiv = createBarDiv();
+        this.element.appendChild(this.barDiv);
 
-        DOM.sinkEvents(getElement(), Event.ONCLICK | Event.ONMOUSEOVER
-                | Event.ONMOUSEOUT | Event.ONFOCUS | Event.ONBLUR
-                | Event.ONKEYUP);
+        DOM.sinkEvents(getElement(), Event.ONCLICK | Event.ONMOUSEOVER | Event.ONMOUSEOUT | Event.ONFOCUS | Event.ONBLUR | Event.ONKEYUP);
     }
 
-    void updateValueCaptions(Map<Integer, String> valueCaptions) {
-        for (Element starElement : starElements) {
-            int rating = starElement.getPropertyInt("rating");
-            String caption = valueCaptions.get(rating);
+    void updateValueCaptions(final Map<Integer, String> valueCaptions) {
+        for (final Element starElement : this.starElements) {
+            final int rating = starElement.getPropertyInt("rating");
+            final String caption = valueCaptions.get(rating);
             if (caption != null) {
                 starElement.setPropertyString("caption", caption);
 
@@ -108,238 +104,254 @@ public class RatingStarsWidget extends FocusWidget implements HasAnimation,
     }
 
     private void createStarElements() {
-        starElements = new Element[maxValue];
-        for (int i = 0; i < maxValue; i++) {
-            DivElement starDiv = createStarDiv(i + 1);
-            starElements[i] = starDiv;
-            element.appendChild(starDiv);
+        this.starElements = new Element[this.maxValue];
+        for (int i = 0; i < this.maxValue; i++) {
+            final DivElement starDiv = createStarDiv(i + 1);
+            this.starElements[i] = starDiv;
+            this.element.appendChild(starDiv);
         }
     }
 
     @Override
-    public void onBrowserEvent(Event event) {
-        if (!isEnabled() || readonly) {
+    public void onBrowserEvent(final Event event) {
+        if (!isEnabled() || this.readonly) {
             return; // Do nothing if disabled or read-only.
         }
 
         super.onBrowserEvent(event);
 
-        Element target = Element.as(event.getEventTarget());
+        final Element target = Element.as(event.getEventTarget());
         switch (DOM.eventGetType(event)) {
-        case Event.ONCLICK:
-            // update value
-            setValueFromElement(target);
-            break;
-        case Event.ONMOUSEOVER:
-            // animate
-            if (target.getClassName().contains(STAR_CLASSNAME)) {
-                int rating = target.getPropertyInt("rating");
-                setFocusIndex(rating - 1);
-                setFocus(true);
-                StarCaptionUtil.showAroundElement(target,
-                        target.getPropertyString("caption"));
-            }
-            break;
-        case Event.ONMOUSEOUT:
-            setBarWidth(calcBarWidth(value));
-            setFocusIndex(-1);
-            StarCaptionUtil.hide();
-            break;
-        case Event.ONFOCUS:
-            getElement().addClassName(WRAPPER_CLASSNAME + "-focus");
-            if (focusIndex < 0) {
-                if (Math.round(value) > 0) {
-                    // focus the current value (or the closest int)
-                    setFocusIndex((int) (Math.round(value) - 1));
-                } else {
-                    // focus the first value
-                    setFocusIndex(0);
+            case Event.ONCLICK:
+                // update value
+                setValueFromElement(target);
+                break;
+            case Event.ONMOUSEOVER:
+                // animate
+                if (target.getClassName()
+                    .contains(RatingStarsWidget.STAR_CLASSNAME)) {
+                    final int rating = target.getPropertyInt("rating");
+                    setFocusIndex(rating - 1);
+                    setFocus(true);
+                    StarCaptionUtil.showAroundElement(target, target.getPropertyString("caption"));
                 }
-            }
-            break;
-        case Event.ONBLUR:
-            getElement().removeClassName(WRAPPER_CLASSNAME + "-focus");
-            setFocusIndex(-1);
-            setBarWidth(calcBarWidth(value));
-            StarCaptionUtil.hide();
-            break;
-        case Event.ONKEYUP:
-            handleKeyUp(event);
-            break;
+                break;
+            case Event.ONMOUSEOUT:
+                setBarWidth(calcBarWidth(this.value));
+                setFocusIndex(-1);
+                StarCaptionUtil.hide();
+                break;
+            case Event.ONFOCUS:
+                getElement().addClassName(RatingStarsWidget.WRAPPER_CLASSNAME + "-focus");
+                if (this.focusIndex < 0) {
+                    if (Math.round(this.value) > 0) {
+                        // focus the current value (or the closest int)
+                        setFocusIndex((int) (Math.round(this.value) - 1));
+                    }
+                    else {
+                        // focus the first value
+                        setFocusIndex(0);
+                    }
+                }
+                break;
+            case Event.ONBLUR:
+                getElement().removeClassName(RatingStarsWidget.WRAPPER_CLASSNAME + "-focus");
+                setFocusIndex(-1);
+                setBarWidth(calcBarWidth(this.value));
+                StarCaptionUtil.hide();
+                break;
+            case Event.ONKEYUP:
+                handleKeyUp(event);
+                break;
         }
     }
 
-    private void setFocusIndex(int index) {
+    private void setFocusIndex(final int index) {
         // remove old focus class
-        if (focusIndex >= 0 && focusIndex < starElements.length) {
-            starElements[focusIndex].removeClassName(STAR_CLASSNAME + "-focus");
+        if (this.focusIndex >= 0 && this.focusIndex < this.starElements.length) {
+            this.starElements[this.focusIndex].removeClassName(RatingStarsWidget.STAR_CLASSNAME + "-focus");
         }
         // update focusIndex and add class
-        focusIndex = index;
-        if (focusIndex >= 0 && focusIndex < starElements.length) {
-            Element focusedStar = starElements[focusIndex];
+        this.focusIndex = index;
+        if (this.focusIndex >= 0 && this.focusIndex < this.starElements.length) {
+            final Element focusedStar = this.starElements[this.focusIndex];
 
-            focusedStar.addClassName(STAR_CLASSNAME + "-focus");
+            focusedStar.addClassName(RatingStarsWidget.STAR_CLASSNAME + "-focus");
             setBarWidth(calcBarWidth(focusedStar.getPropertyInt("rating")));
-            StarCaptionUtil.showAroundElement(focusedStar,
-                    focusedStar.getPropertyString("caption"));
+            StarCaptionUtil.showAroundElement(focusedStar, focusedStar.getPropertyString("caption"));
         }
     }
 
-    private void changeFocusIndex(int delta) {
-        int newFocusIndex = focusIndex + delta;
+    private void changeFocusIndex(final int delta) {
+        final int newFocusIndex = this.focusIndex + delta;
 
         // check for boundaries
-        if (newFocusIndex >= 0 && newFocusIndex < starElements.length) {
+        if (newFocusIndex >= 0 && newFocusIndex < this.starElements.length) {
             setFocusIndex(newFocusIndex);
         }
     }
 
-    private void setValueFromElement(Element target) {
-        if (target.getClassName().contains(STAR_CLASSNAME)) {
-            int ratingValue = target.getPropertyInt("rating");
+    private void setValueFromElement(final Element target) {
+        if (target.getClassName()
+            .contains(RatingStarsWidget.STAR_CLASSNAME)) {
+            final int ratingValue = target.getPropertyInt("rating");
             setValue((double) ratingValue, true);
         }
     }
 
-    public void handleKeyUp(Event event) {
+    public void handleKeyUp(final Event event) {
         if (event.getKeyCode() == KeyCodes.KEY_RIGHT) {
             changeFocusIndex(+1);
-        } else if (event.getKeyCode() == KeyCodes.KEY_LEFT) {
+        }
+        else if (event.getKeyCode() == KeyCodes.KEY_LEFT) {
             changeFocusIndex(-1);
-        } else if (event.getKeyCode() == KeyCodes.KEY_ENTER) {
-            setValueFromElement(starElements[focusIndex]);
+        }
+        else if (event.getKeyCode() == KeyCodes.KEY_ENTER) {
+            setValueFromElement(this.starElements[this.focusIndex]);
         }
     }
 
     /**
      * Creates the DivElement of the bar representing the current value.
-     * 
+     *
      * @return the newly created DivElement representing the bar.
      * @see #setBarWidth(byte)
      */
     private Element createBarDiv() {
-        DivElement barDiv = Document.get().createDivElement();
-        barDiv.setClassName(BAR_CLASSNAME);
-        barDiv.getStyle().setProperty("width", calcBarWidth(value) + "%");
+        final DivElement barDiv = Document.get()
+            .createDivElement();
+        barDiv.setClassName(RatingStarsWidget.BAR_CLASSNAME);
+        barDiv.getStyle()
+            .setProperty("width", calcBarWidth(this.value) + "%");
         return barDiv;
     }
 
     /**
-     * Sets the width of the bar div instantly or via animated progress
-     * depending on the value of the <code>animated</code> property.
+     * Sets the width of the bar div instantly or via animated progress depending on the value of the <code>animated</code> property.
      */
     private void setBarWidth(final byte widthPercentage) {
-        if (barDiv == null) {
+        if (this.barDiv == null) {
             return;
         }
 
         final byte currentWidthPercentage = getCurrentBarWidth();
         if (currentWidthPercentage != widthPercentage) {
             if (!isAnimationEnabled()) {
-                barDiv.getStyle().setProperty("width", widthPercentage + "%");
-            } else {
-                Animation animation = new Animation() {
+                this.barDiv.getStyle()
+                    .setProperty("width", widthPercentage + "%");
+            }
+            else {
+                final Animation animation = new Animation() {
                     @Override
-                    protected void onUpdate(double progress) {
-                        byte newWidth = (byte) (currentWidthPercentage + (progress * (widthPercentage - currentWidthPercentage)));
-                        barDiv.getStyle().setProperty("width", newWidth + "%");
+                    protected void onUpdate(final double progress) {
+                        final byte newWidth = (byte) (currentWidthPercentage + (progress * (widthPercentage - currentWidthPercentage)));
+                        RatingStarsWidget.this.barDiv.getStyle()
+                            .setProperty("width", newWidth + "%");
                     }
                 };
-                animation.run(ANIMATION_DURATION_IN_MS);
+                animation.run(RatingStarsWidget.ANIMATION_DURATION_IN_MS);
             }
         }
     }
 
     private byte getCurrentBarWidth() {
-        String currentWidth = barDiv.getStyle().getProperty("width");
+        final String currentWidth = this.barDiv.getStyle()
+            .getProperty("width");
         byte currentWidthPercentage = 0;
         if (currentWidth != null && currentWidth.length() > 0) {
-            currentWidthPercentage = Byte.valueOf(currentWidth.substring(0,
-                    currentWidth.length() - 1));
+            currentWidthPercentage = Byte.valueOf(currentWidth.substring(0, currentWidth.length() - 1));
         }
         return currentWidthPercentage;
     }
 
     /**
-     * Calculates the bar width for the given <code>forValue</code> as a
-     * percentage of the <code>maxValue</code>. Returned value is from 0 to 100.
-     * 
+     * Calculates the bar width for the given <code>forValue</code> as a percentage of the <code>maxValue</code>. Returned value is from 0 to 100.
+     *
      * @return width percentage (0..100)
      */
-    private byte calcBarWidth(double forValue) {
-        return (byte) (forValue * 100 / maxValue);
+    private byte calcBarWidth(final double forValue) {
+        return (byte) (forValue * 100 / this.maxValue);
     }
 
     /**
-     * Creates a DivElement representing a single star. Given
-     * <code>rating</code> value is set as an int property for the div.
-     * 
-     * @param rating
-     *            rating value of this star.
+     * Creates a DivElement representing a single star. Given <code>rating</code> value is set as an int property for the div.
+     *
+     * @param rating rating value of this star.
      * @return a DivElement representing a single star.
      */
-    private DivElement createStarDiv(int rating) {
-        DivElement starDiv = Document.get().createDivElement();
-        starDiv.setClassName(STAR_CLASSNAME);
+    private DivElement createStarDiv(final int rating) {
+        final DivElement starDiv = Document.get()
+            .createDivElement();
+        starDiv.setClassName(RatingStarsWidget.STAR_CLASSNAME);
         starDiv.setPropertyInt("rating", rating);
         return starDiv;
     }
 
     @Override
     public boolean isAnimationEnabled() {
-        return animated;
+        return this.animated;
     }
 
     @Override
-    public void setAnimationEnabled(boolean enable) {
+    public void setAnimationEnabled(final boolean enable) {
         this.animated = enable;
     }
 
-    public void setMaxValue(int maxValue) {
+    public void setMaxValue(final int maxValue) {
         if (this.maxValue != maxValue) {
             this.maxValue = maxValue;
             initDom(); // Recreate the DOM.
         }
     }
 
-    private void internalSetValue(double value) {
-        if (this.value != value) {
-            this.value = value;
-            setBarWidth(calcBarWidth(this.value));
-        }
+    private void internalSetValue(final double value) {
+        this.value = value;
+        setBarWidth(calcBarWidth(this.value));
     }
 
-    public void setReadOnly(boolean readonly) {
+    public void setReadOnly(final boolean readonly) {
         if (this.readonly != readonly) {
             this.readonly = readonly;
         }
     }
 
     @Override
-    public HandlerRegistration addValueChangeHandler(
-            ValueChangeHandler<Double> handler) {
+    public HandlerRegistration addValueChangeHandler(final ValueChangeHandler<Double> handler) {
         return addHandler(handler, ValueChangeEvent.getType());
     }
 
     @Override
     public Double getValue() {
-        return value;
+        return this.value;
     }
 
     @Override
-    public void setValue(Double value) {
-        setValue(value, false);
+    public void setValue(final Double value) {
+        // only for reseting value; otherwise rpc call would be done twice and the value would always be reseted
+        if (value.intValue() == 0) {
+            setValue(0.0, false);
+        }
     }
 
     @Override
-    public void setValue(Double value, boolean fireEvents) {
+    public void setValue(Double value, final boolean fireEvents) {
         // Null not supported -> convert to zero.
         if (value == null) {
             value = 0.0;
         }
 
-        ValueChangeEvent.fireIfNotEqual(this, this.value, value);
+        if (selectedValueChanged(value)) {
+            ValueChangeEvent.fire(this, value);
+        }
+        else {
+            // the current value was re-clicked; reset value to 0.0
+            ValueChangeEvent.fire(this, 0.0);
+        }
+
         internalSetValue(value);
+    }
+
+    private boolean selectedValueChanged(final Double value) {
+        return ValueChangeEvent.getType() != null && new Double(this.value) != value && (getValue() == null || !getValue().equals(value));
     }
 }
