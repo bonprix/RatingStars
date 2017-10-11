dcdea6bf99b803540c39f3a343384d653af13a05
vagharshakus <vagharshak.baghdasaryan@bonprix.net>
2017-10-11 14:13:03 +0200

    FW-334: created bp component; project is now vaddin 7 and maven

---
+++ ratingstars-demo/src/main/java/org/vaadin/teemu/ratingstars/demo/RatingStarsDemo.java
@@ -0,0 +1,244 @@
+package org.vaadin.teemu.ratingstars.demo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.vaadin.teemu.ratingstars.RatingStars;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * A demo application for the RatingStars component. For a live demo see
+ * <a href="http://teemu.virtuallypreinstalled.com/RatingStars">http://teemu.virtuallypreinstalled.com/RatingStars</a>
+ *
+ * @author Teemu Pöntelin
+ */
+@Theme("valo")
+@Title("RatingStars Component Demo")
+@SuppressWarnings("unused")
+public class RatingStarsDemo extends UI {
+    private static final long serialVersionUID = 7705972095201251401L;
+
+    private Table table;
+    private CheckBox animatedCheckBox;
+
+    @WebServlet(
+        urlPatterns = "/*",
+        name = "MyUIServlet",
+        asyncSupported = true)
+    @VaadinServletConfiguration(
+        ui = RatingStarsDemo.class,
+        productionMode = false,
+        widgetset = "org.vaadin.teemu.ratingstars.demo.DemoWidgetSet")
+    public static class MyUIServlet extends VaadinServlet {
+    }
+
+    private final static Map<Integer, String> valueCaptions = new HashMap<>(5, 1);
+
+    static {
+        RatingStarsDemo.valueCaptions.put(1, "Epic Fail");
+        RatingStarsDemo.valueCaptions.put(2, "Poor");
+        RatingStarsDemo.valueCaptions.put(3, "OK");
+        RatingStarsDemo.valueCaptions.put(4, "Good");
+        RatingStarsDemo.valueCaptions.put(5, "Excellent");
+    }
+
+    private static final String[] movieNames = { "The Matrix", "Memento", "Kill Bill: Vol. 1" };
+
+    private final Set<RatingStars> allRatingStars = new HashSet<>();
+
+    private VerticalLayout mainLayout;
+
+    @Override
+    protected void init(final VaadinRequest request) {
+        initWindowAndDescription();
+        initDemoPanel();
+    }
+
+    private void initWindowAndDescription() {
+        final VerticalLayout centerLayout = new VerticalLayout();
+
+        this.mainLayout = new VerticalLayout();
+        final Panel mainPanel = new Panel(this.mainLayout);
+        mainPanel.setWidth("750px");
+        centerLayout.addComponent(mainPanel);
+        centerLayout.setComponentAlignment(mainPanel, Alignment.TOP_CENTER);
+        setContent(centerLayout);
+
+        final StringBuilder descriptionXhtml = new StringBuilder();
+        descriptionXhtml.append("<h1 style=\"margin: 0;\">RatingStars Component Demo</h1>");
+        descriptionXhtml.append("<p>RatingStars is a simple component for giving rating values.</p>");
+        descriptionXhtml.append("<p>Download and rate this component at <a href=\"http://vaadin.com/addon/ratingstars\">Vaadin Directory</a>. ");
+        descriptionXhtml.append("Get the source code at <a href=\"https://github.com/tehapo/RatingStars\">GitHub</a>.</p>");
+        descriptionXhtml.append("<p>Highlights:</p>");
+        descriptionXhtml.append("<ul>");
+        descriptionXhtml.append("<li>Keyboard usage (focus with tab, navigate with arrow keys, select with enter)</li>");
+        descriptionXhtml.append("<li>Easily customizable appearance</li>");
+        descriptionXhtml.append("<li>Captions for individual values</li>");
+        descriptionXhtml.append("<li>Optional transition animations</li>");
+        descriptionXhtml.append("</ul>");
+        descriptionXhtml.append("<div style=\"height: 10px\"></div>");
+
+        final Label description = new Label(descriptionXhtml.toString(), ContentMode.HTML);
+        this.mainLayout.addComponent(description);
+    }
+
+    private void initDemoPanel() {
+        final Panel demoPanel = new Panel("Demonstration");
+        final VerticalLayout demoLayout = new VerticalLayout();
+        demoLayout.setSpacing(true);
+        demoLayout.setMargin(true);
+        demoPanel.setContent(demoLayout);
+        this.mainLayout.addComponent(demoPanel);
+
+        // animated checkbox
+        this.animatedCheckBox = new CheckBox("Animated?");
+        this.animatedCheckBox.setValue(true);
+        this.animatedCheckBox.setImmediate(true);
+        this.animatedCheckBox.addValueChangeListener(new ValueChangeListener() {
+
+            private static final long serialVersionUID = 6001160591512323325L;
+
+            @Override
+            public void valueChange(final ValueChangeEvent event) {
+                for (final RatingStars rs : RatingStarsDemo.this.allRatingStars) {
+                    rs.setAnimated((Boolean) event.getProperty()
+                        .getValue());
+                }
+            }
+        });
+        demoLayout.addComponent(this.animatedCheckBox);
+
+        // create and populate the movie table
+        this.table = new Table("Rate your favourite movies");
+        this.table.addContainerProperty("Movie", String.class, null);
+        this.table.addContainerProperty("Comment", TextField.class, null);
+        this.table.addContainerProperty("Your rating", RatingStars.class, null);
+        this.table.addContainerProperty("Average rating", RatingStars.class, null);
+        populateTable();
+        this.table.setPageLength(this.table.getItemIds()
+            .size());
+        demoLayout.addComponent(this.table);
+
+        // theme demos
+        demoLayout.addComponent(new Label("<strong>The component has two built-in styles.</strong>", ContentMode.HTML));
+        final RatingStars defaultRs = new RatingStars();
+        defaultRs.setDescription("Default RatingStars");
+        defaultRs.setCaption("default");
+        this.allRatingStars.add(defaultRs);
+
+        final RatingStars tinyRs = new RatingStars();
+        tinyRs.setMaxValue(3);
+        tinyRs.setStyleName("tiny");
+        tinyRs.setCaption("tiny");
+        this.allRatingStars.add(tinyRs);
+
+        final HorizontalLayout themeLayout = new HorizontalLayout();
+        themeLayout.setSpacing(true);
+        themeLayout.addComponent(defaultRs);
+        themeLayout.addComponent(tinyRs);
+        demoLayout.addComponent(themeLayout);
+
+        // component states
+        demoLayout.addComponent(new Label("<strong>Component states</strong>", ContentMode.HTML));
+        final RatingStars disabledRs = new RatingStars();
+        disabledRs.setCaption("disabled");
+        disabledRs.setValue(2.5);
+        disabledRs.setEnabled(false);
+
+        final RatingStars readonlyRs = new RatingStars();
+        readonlyRs.setCaption("read-only");
+        readonlyRs.setValue(2.5);
+        readonlyRs.setReadOnly(true);
+
+        final HorizontalLayout stateLayout = new HorizontalLayout();
+        stateLayout.setSpacing(true);
+        stateLayout.addComponent(disabledRs);
+        stateLayout.addComponent(readonlyRs);
+        demoLayout.addComponent(stateLayout);
+    }
+
+    /**
+     * Populate the table with some random data.
+     */
+    @SuppressWarnings("unchecked")
+    private void populateTable() {
+        final Random r = new Random();
+        for (final String movieName : RatingStarsDemo.movieNames) {
+            final TextField textField = new TextField();
+
+            final RatingStars avgRs = new RatingStars();
+            avgRs.setMaxValue(5);
+            avgRs.setValue(r.nextDouble() * 4 + 1);
+            avgRs.setReadOnly(true);
+            this.allRatingStars.add(avgRs);
+
+            final RatingStars yourRs = new RatingStars();
+            yourRs.setMaxValue(5);
+            yourRs.setImmediate(true);
+            yourRs.setValueCaption(RatingStarsDemo.valueCaptions.values()
+                .toArray(new String[5]));
+            yourRs.addValueChangeListener(new Property.ValueChangeListener() {
+
+                private static final long serialVersionUID = 3978380217446180197L;
+
+                @Override
+                public void valueChange(final ValueChangeEvent event) {
+                    final Double value = (Double) event.getProperty()
+                        .getValue();
+
+                    Notification.show("You voted " + value + " stars for " + movieName + ".", Notification.Type.TRAY_NOTIFICATION);
+
+                    final RatingStars changedRs = (RatingStars) event.getProperty();
+                    // reset value captions
+                    changedRs.setValueCaption(RatingStarsDemo.valueCaptions.values()
+                        .toArray(new String[5]));
+                    // set "Your Rating" caption
+                    changedRs.setValueCaption((int) Math.round(value), "Your Rating");
+
+                    // dummy logic to calculate "average" value
+                    avgRs.setReadOnly(false);
+                    avgRs.setValue(((avgRs.getValue()) + value) / 2);
+                    avgRs.setReadOnly(true);
+                }
+            });
+            this.allRatingStars.add(yourRs);
+
+            final Object itemId = this.table.addItem();
+            final Item i = this.table.getItem(itemId);
+            i.getItemProperty("Movie")
+                .setValue(movieName);
+            i.getItemProperty("Comment")
+                .setValue(textField);
+            i.getItemProperty("Your rating")
+                .setValue(yourRs);
+            i.getItemProperty("Average rating")
+                .setValue(avgRs);
+        }
+    }
+
+}
---
+++ ratingstars/src/main/java/org/vaadin/teemu/ratingstars/gwt/client/RatingStarsWidget.java
@@ -0,0 +1,357 @@
+package org.vaadin.teemu.ratingstars.gwt.client;
+
+import java.util.Map;
+
+import com.google.gwt.animation.client.Animation;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.HasAnimation;
+import com.google.gwt.user.client.ui.HasValue;
+
+/**
+ * RatingStarsWidget is the client-side implementation of the RatingStars component.
+ *
+ * The DOM tree for this component is constructed as follows:
+ *
+ * <pre>
+ *    div.v-ratingstars-wrapper
+ *        div.v-ratingstars
+ *            div.v-ratingstars-star
+ *            div.v-ratingstars-star
+ *            div.v-ratingstars-star
+ *            ...
+ *            div.v-ratingstars-star
+ *            div.v-ratingstars-bar
+ * </pre>
+ *
+ * The idea behind the DOM tree is that {@code .v-ratingstars-star} elements always have a partially transparent background image and the width of the
+ * {@code .v-ratingstars-bar} element behind these star elements is changed according to the current value.
+ *
+ * @author Teemu Pöntelin
+ */
+public class RatingStarsWidget extends FocusWidget implements HasAnimation, HasValue<Double>, HasValueChangeHandlers<Double> {
+
+    /** Set the CSS class names to allow styling. */
+    public static final String CLASSNAME = "v-ratingstars";
+    public static final String STAR_CLASSNAME = RatingStarsWidget.CLASSNAME + "-star";
+    public static final String BAR_CLASSNAME = RatingStarsWidget.CLASSNAME + "-bar";
+    public static final String WRAPPER_CLASSNAME = RatingStarsWidget.CLASSNAME + "-wrapper";
+
+    private static final int ANIMATION_DURATION_IN_MS = 150;
+
+    // DOM elements
+    private Element barDiv;
+    private Element element;
+    private Element[] starElements;
+
+    /** Currently focused star (by keyboard focus). */
+    private int focusIndex = -1;
+
+    private int maxValue = 5;
+    private double value;
+
+    private boolean animated;
+    private boolean readonly;
+
+    public RatingStarsWidget() {
+        setElement(Document.get()
+            .createDivElement());
+        setStyleName(RatingStarsWidget.WRAPPER_CLASSNAME);
+        initDom();
+    }
+
+    private void initDom() {
+        if (this.element != null) {
+            // Remove previous element.
+            getElement().removeChild(this.element);
+        }
+
+        this.element = Document.get()
+            .createDivElement();
+        this.element.setClassName(RatingStarsWidget.CLASSNAME);
+        getElement().appendChild(this.element);
+
+        createStarElements();
+
+        this.barDiv = createBarDiv();
+        this.element.appendChild(this.barDiv);
+
+        DOM.sinkEvents(getElement(), Event.ONCLICK | Event.ONMOUSEOVER | Event.ONMOUSEOUT | Event.ONFOCUS | Event.ONBLUR | Event.ONKEYUP);
+    }
+
+    void updateValueCaptions(final Map<Integer, String> valueCaptions) {
+        for (final Element starElement : this.starElements) {
+            final int rating = starElement.getPropertyInt("rating");
+            final String caption = valueCaptions.get(rating);
+            if (caption != null) {
+                starElement.setPropertyString("caption", caption);
+
+                if (StarCaptionUtil.isVisibleForStarElement(starElement)) {
+                    // update currently visible caption
+                    StarCaptionUtil.showAroundElement(starElement, caption);
+                }
+            }
+        }
+    }
+
+    private void createStarElements() {
+        this.starElements = new Element[this.maxValue];
+        for (int i = 0; i < this.maxValue; i++) {
+            final DivElement starDiv = createStarDiv(i + 1);
+            this.starElements[i] = starDiv;
+            this.element.appendChild(starDiv);
+        }
+    }
+
+    @Override
+    public void onBrowserEvent(final Event event) {
+        if (!isEnabled() || this.readonly) {
+            return; // Do nothing if disabled or read-only.
+        }
+
+        super.onBrowserEvent(event);
+
+        final Element target = Element.as(event.getEventTarget());
+        switch (DOM.eventGetType(event)) {
+            case Event.ONCLICK:
+                // update value
+                setValueFromElement(target);
+                break;
+            case Event.ONMOUSEOVER:
+                // animate
+                if (target.getClassName()
+                    .contains(RatingStarsWidget.STAR_CLASSNAME)) {
+                    final int rating = target.getPropertyInt("rating");
+                    setFocusIndex(rating - 1);
+                    setFocus(true);
+                    StarCaptionUtil.showAroundElement(target, target.getPropertyString("caption"));
+                }
+                break;
+            case Event.ONMOUSEOUT:
+                setBarWidth(calcBarWidth(this.value));
+                setFocusIndex(-1);
+                StarCaptionUtil.hide();
+                break;
+            case Event.ONFOCUS:
+                getElement().addClassName(RatingStarsWidget.WRAPPER_CLASSNAME + "-focus");
+                if (this.focusIndex < 0) {
+                    if (Math.round(this.value) > 0) {
+                        // focus the current value (or the closest int)
+                        setFocusIndex((int) (Math.round(this.value) - 1));
+                    }
+                    else {
+                        // focus the first value
+                        setFocusIndex(0);
+                    }
+                }
+                break;
+            case Event.ONBLUR:
+                getElement().removeClassName(RatingStarsWidget.WRAPPER_CLASSNAME + "-focus");
+                setFocusIndex(-1);
+                setBarWidth(calcBarWidth(this.value));
+                StarCaptionUtil.hide();
+                break;
+            case Event.ONKEYUP:
+                handleKeyUp(event);
+                break;
+        }
+    }
+
+    private void setFocusIndex(final int index) {
+        // remove old focus class
+        if (this.focusIndex >= 0 && this.focusIndex < this.starElements.length) {
+            this.starElements[this.focusIndex].removeClassName(RatingStarsWidget.STAR_CLASSNAME + "-focus");
+        }
+        // update focusIndex and add class
+        this.focusIndex = index;
+        if (this.focusIndex >= 0 && this.focusIndex < this.starElements.length) {
+            final Element focusedStar = this.starElements[this.focusIndex];
+
+            focusedStar.addClassName(RatingStarsWidget.STAR_CLASSNAME + "-focus");
+            setBarWidth(calcBarWidth(focusedStar.getPropertyInt("rating")));
+            StarCaptionUtil.showAroundElement(focusedStar, focusedStar.getPropertyString("caption"));
+        }
+    }
+
+    private void changeFocusIndex(final int delta) {
+        final int newFocusIndex = this.focusIndex + delta;
+
+        // check for boundaries
+        if (newFocusIndex >= 0 && newFocusIndex < this.starElements.length) {
+            setFocusIndex(newFocusIndex);
+        }
+    }
+
+    private void setValueFromElement(final Element target) {
+        if (target.getClassName()
+            .contains(RatingStarsWidget.STAR_CLASSNAME)) {
+            final int ratingValue = target.getPropertyInt("rating");
+            setValue((double) ratingValue, true);
+        }
+    }
+
+    public void handleKeyUp(final Event event) {
+        if (event.getKeyCode() == KeyCodes.KEY_RIGHT) {
+            changeFocusIndex(+1);
+        }
+        else if (event.getKeyCode() == KeyCodes.KEY_LEFT) {
+            changeFocusIndex(-1);
+        }
+        else if (event.getKeyCode() == KeyCodes.KEY_ENTER) {
+            setValueFromElement(this.starElements[this.focusIndex]);
+        }
+    }
+
+    /**
+     * Creates the DivElement of the bar representing the current value.
+     *
+     * @return the newly created DivElement representing the bar.
+     * @see #setBarWidth(byte)
+     */
+    private Element createBarDiv() {
+        final DivElement barDiv = Document.get()
+            .createDivElement();
+        barDiv.setClassName(RatingStarsWidget.BAR_CLASSNAME);
+        barDiv.getStyle()
+            .setProperty("width", calcBarWidth(this.value) + "%");
+        return barDiv;
+    }
+
+    /**
+     * Sets the width of the bar div instantly or via animated progress depending on the value of the <code>animated</code> property.
+     */
+    private void setBarWidth(final byte widthPercentage) {
+        if (this.barDiv == null) {
+            return;
+        }
+
+        final byte currentWidthPercentage = getCurrentBarWidth();
+        if (currentWidthPercentage != widthPercentage) {
+            if (!isAnimationEnabled()) {
+                this.barDiv.getStyle()
+                    .setProperty("width", widthPercentage + "%");
+            }
+            else {
+                final Animation animation = new Animation() {
+                    @Override
+                    protected void onUpdate(final double progress) {
+                        final byte newWidth = (byte) (currentWidthPercentage + (progress * (widthPercentage - currentWidthPercentage)));
+                        RatingStarsWidget.this.barDiv.getStyle()
+                            .setProperty("width", newWidth + "%");
+                    }
+                };
+                animation.run(RatingStarsWidget.ANIMATION_DURATION_IN_MS);
+            }
+        }
+    }
+
+    private byte getCurrentBarWidth() {
+        final String currentWidth = this.barDiv.getStyle()
+            .getProperty("width");
+        byte currentWidthPercentage = 0;
+        if (currentWidth != null && currentWidth.length() > 0) {
+            currentWidthPercentage = Byte.valueOf(currentWidth.substring(0, currentWidth.length() - 1));
+        }
+        return currentWidthPercentage;
+    }
+
+    /**
+     * Calculates the bar width for the given <code>forValue</code> as a percentage of the <code>maxValue</code>. Returned value is from 0 to 100.
+     *
+     * @return width percentage (0..100)
+     */
+    private byte calcBarWidth(final double forValue) {
+        return (byte) (forValue * 100 / this.maxValue);
+    }
+
+    /**
+     * Creates a DivElement representing a single star. Given <code>rating</code> value is set as an int property for the div.
+     *
+     * @param rating rating value of this star.
+     * @return a DivElement representing a single star.
+     */
+    private DivElement createStarDiv(final int rating) {
+        final DivElement starDiv = Document.get()
+            .createDivElement();
+        starDiv.setClassName(RatingStarsWidget.STAR_CLASSNAME);
+        starDiv.setPropertyInt("rating", rating);
+        return starDiv;
+    }
+
+    @Override
+    public boolean isAnimationEnabled() {
+        return this.animated;
+    }
+
+    @Override
+    public void setAnimationEnabled(final boolean enable) {
+        this.animated = enable;
+    }
+
+    public void setMaxValue(final int maxValue) {
+        if (this.maxValue != maxValue) {
+            this.maxValue = maxValue;
+            initDom(); // Recreate the DOM.
+        }
+    }
+
+    private void internalSetValue(final double value) {
+        this.value = value;
+        setBarWidth(calcBarWidth(this.value));
+    }
+
+    public void setReadOnly(final boolean readonly) {
+        if (this.readonly != readonly) {
+            this.readonly = readonly;
+        }
+    }
+
+    @Override
+    public HandlerRegistration addValueChangeHandler(final ValueChangeHandler<Double> handler) {
+        return addHandler(handler, ValueChangeEvent.getType());
+    }
+
+    @Override
+    public Double getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void setValue(final Double value) {
+        // only for reseting value; otherwise rpc call would be done twice and the value would always be reseted
+        if (value.intValue() == 0) {
+            setValue(0.0, false);
+        }
+    }
+
+    @Override
+    public void setValue(Double value, final boolean fireEvents) {
+        // Null not supported -> convert to zero.
+        if (value == null) {
+            value = 0.0;
+        }
+
+        if (selectedValueChanged(value)) {
+            ValueChangeEvent.fire(this, value);
+        }
+        else {
+            // the current value was re-clicked; reset value to 0.0
+            ValueChangeEvent.fire(this, 0.0);
+        }
+
+        internalSetValue(value);
+    }
+
+    private boolean selectedValueChanged(final Double value) {
+        return ValueChangeEvent.getType() != null && new Double(this.value) != value && (getValue() == null || !getValue().equals(value));
+    }
+}
